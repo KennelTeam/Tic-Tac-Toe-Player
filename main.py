8d3fc0eb8ee32df08bb0ab78a0cbee510fc0cbe2
Alexey <alexey.nebolsin.jr@gmail.com>
2021-08-11 22:23:41 +0300

    Learning from big data works

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import json
+import os
 import random
 from tqdm import tqdm
 
 from Learning.game import Game
 from NNStructure.simple_neuro.facade import SimpleNeuroFacade
 from utils.player_role import PlayerRole
-
+os.chdir('TicTacToe')
+print(os.getcwd())
 batch_size = 10
-
-gamesJson = json.loads(open('data.json').read())
+print('Starting reading')
+gamesJson = json.loads(open('C:/Users/alexe/Documents/Hackatons/TicTacToe/data.json').read())
 net = SimpleNeuroFacade(name='N1')
-
+print('Starting learning')
 for batch_index in tqdm(range(len(gamesJson) // batch_size)):
     batch = gamesJson[batch_index * batch_size: (batch_index + 1) * batch_size]
     structGames = []
