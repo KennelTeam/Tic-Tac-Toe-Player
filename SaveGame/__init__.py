be402eaaa3f3b41931eb496eae56fa23bd3ddce9
yveega <veega@gmail.com>
2021-08-12 03:17:31 +0300

    Add SaveGame module

---
+++ SaveGame/__init__.py
@@ -0,0 +1,12 @@
+import pickle
+from Learning.game import Game
+
+def save_game(game: Game, filename='game1.pkl'):
+    with open('Games/' + filename, 'wb') as file:
+        pickle.dump(game, file, pickle.HIGHEST_PROTOCOL)
+
+
+def import_game(filename='game1.pkl'):
+    with open('Games/' + filename, 'rb') as file:
+        game = pickle.load(file)
+    return game
